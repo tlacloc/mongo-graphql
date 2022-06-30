3a8cee06d311a279dd79834e537fd801297dba2c
Erick Casanova <desneruda@gmail.com>
2022-06-29 22:16:59 -0500

    basic schema

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { graphql, buildSchema } = require('graphql');
+
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+    }
+`);
+
+graphql({
+    schema: schema,
+    source: '{ hello }'})
+.then((data) => {
+    console.log(data);
+})
+.catch(e => {
+    console.log(e);
+});
